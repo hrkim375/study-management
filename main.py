eb2e81890822dda592b00c3ef345e3c6e9dcedf3
hrkim375 <hrkim375@gmail.com>
2017-04-02 23:56:14 +0900

    Add 'menu' label to main window

--- main.py
+++ main.py
@@ -6,28 +6,31 @@ class Main(Frame):
     def __init__(self, master=None):
         super().__init__(master)
         
-        self.width = master.winfo_screenwidth()
+        self.width = 30
         
         self.pack()
         self.createWidgets()
 
     def createWidgets(self):
 
+        # menu Label
+        self.menuLabel = Label(self, text="Menu")
+        self.menuLabel.grid(row=0)
+
         # menu1 Study Time
-        self.menu1 = Button(self, text="1. Input your study time", width=self.width, height=2)
-        self.menu1.pack(side="top")
+        self.menu1 = Button(self, text="1. Input your study time", width=self.width)
+        self.menu1.grid(row=1)
 
         # menu2 Schedule
-        self.menu2 = Button(self, text="2. Study schedule management", width=self.width, height=2)
-        self.menu2.pack(side="top")
+        self.menu2 = Button(self, text="2. Study schedule management", width=self.width)
+        self.menu2.grid(row=2)
         
         # menu3 Status
-        self.menu3 = Button(self, text="3. Print study status", width=self.width, height=2)
-        self.menu3.pack(side="top")
+        self.menu3 = Button(self, text="3. Print study status", width=self.width)
+        self.menu3.grid(row=3)
 
 root = Tk()
 root.title("Study Management")
-root.geometry("300x150")
 root.resizable(width=False, height=False)
 main = Main(master=root)
 main.mainloop()
